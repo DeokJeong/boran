58bca6ddb7c351a54e4566d78d361cfc77256a63
deokjeong <sp10051993@gmail.com>
2022-01-07 13:51:06 +0900

    배너슬라이드 jQuery 추가

--- link/dog1/js/dog1.js
+++ link/dog1/js/dog1.js
@@ -1,27 +1,65 @@
 $(document).ready(function(){
-    var num = 1;
+    var button = $('#buttonList > li'); 
+    var img = $('.event > ul > li'); 
+    var current = 0; 
+    var lfbtn = $('button.leftbutton') 
+    var rgbtn = $('button.rightbutton') 
+    
+    button.click(function(){ 
+    	var tg = $(this); 
+    	var i = tg.index(); 
+    	
+    	button.removeClass('on'); 
+    	tg.addClass('on'); 
+    	
+    	move(i); 
+    });
+    
+    function move(i){ 
+    	img.eq(current).css('left', 0).stop().animate({left:'-100%'},1000); 
+    	img.eq(i).css('left', '100%').stop().animate({left:0},1000); 
+    	
+    	current = i; 
+    }
 
-    setInterval(function(){
-        if(num < 4){
-            num++;
-            $('.event > ul').animate({
-                left:'-=100%'
-            },'slow')
-        }else{
-            num = 1;
-            $('.event > ul').animate({
-                left:0
-            },'slow')
+    lfbtn.click(function(){
+        button.removeClass("on");
+
+        if(current - 1 < 0){ 
+            
+            button.eq(button.size() - 1).addClass("on"); 
+
+            img.eq(current).css('left', 0).stop().animate({left:'100%'},1000); 
+            img.eq(button.size() - 1).css('left', '-100%').stop().animate({left:0},1000);
+
+            current = button.size() - 1
+        } else { 
+            button.eq(current - 1).addClass("on"); 
+
+            img.eq(current).css('left', 0).stop().animate({left:'100%'},1000); 
+            img.eq(current - 1).css('left', '-100%').stop().animate({left:0},1000); 
+
+            current = current - 1
         }
-    },5000)
+    });
 
-    $('.btn a').click(function(){
-        $('.btn a').removeClass('on');
-        $(this).addClass('on');
+    rgbtn.click(function(){
+        button.removeClass("on"); 
+        
+        if(current + 1 == button.size()){ 
+            button.eq(0).addClass("on");  
 
-        var i = $(this).parent().index();
+            img.eq(current).css('left', 0).stop().animate({left:'-100%'},1000); 
+            img.eq(0).css('left', '100%').stop().animate({left:0},1000); 
+            
+            current = 0 
+        } else { 
+            button.eq(current + 1).addClass("on"); 
 
-        $('.bwrap > div').fadeOut(0);
-        $('.bwrap > div').eq(i).fadeIn(0);
+            img.eq(current).css('left', 0).stop().animate({left:'-100%'},1000);
+            img.eq(current + 1).css('left', '100%').stop().animate({left:0},1000); 
+
+            current = current + 1 
+        }
     });
 })
