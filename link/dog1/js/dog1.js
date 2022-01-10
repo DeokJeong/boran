40043ebd9e2fd059ca7a4f885891513bab74f103
deokjeong <sp10051993@gmail.com>
2022-01-10 10:45:16 +0900

    수정

--- link/dog1/js/dog1.js
+++ link/dog1/js/dog1.js
@@ -4,6 +4,28 @@ $(document).ready(function(){
     var current = 0; 
     var lfbtn = $('button.leftbutton') 
     var rgbtn = $('button.rightbutton') 
+
+    
+    setInterval(function(){
+        if(current + 1 == button.size()){ 
+            button.removeClass('on'); 
+            button.eq(0).addClass("on");  
+
+            img.eq(current).css('left', 0).stop().animate({left:'-100%'},1000); 
+            img.eq(0).css('left', '100%').stop().animate({left:0},1000); 
+            
+            current = 0 
+
+        } else { 
+            button.removeClass('on'); 
+            button.eq(current + 1).addClass("on"); 
+
+            img.eq(current).css('left', 0).stop().animate({left:'-100%'},1000);
+            img.eq(current + 1).css('left', '100%').stop().animate({left:0},1000); 
+
+            current = current + 1 
+        }
+    },5000)
     
     button.click(function(){ 
     	var tg = $(this); 
@@ -61,5 +83,15 @@ $(document).ready(function(){
 
             current = current + 1 
         }
+    });
+
+    $('.btn a').click(function(){
+        $('.btn a').removeClass('on');
+        $(this).addClass('on');
+
+        var i = $(this).parent().index();
+
+        $('.bwrap > div').fadeOut(0);
+        $('.bwrap > div').eq(i).fadeIn(0);
     });
 })
